8f51c302e9ff294cb112c695aaf0ddc102828cf8
Mariopq10 <mario.pq10@gmail.com>
2023-05-25 14:49:36 +0200

    a

--- entrega/tarkov.js
+++ entrega/tarkov.js
@@ -21,3 +21,17 @@ function guardarNombreUsuario() {
     // Actualizar el elemento #nombre-usuario con el nuevo nombre
     document.getElementById('nombre-usuario').textContent = nuevoNombre;
 }
+
+function aplicarCambios() {
+    // mostramos en consola los valores recogidos en la pagina
+    var color = document.getElementsByName('colorElegido')[0];
+    console.log('color elegido ' + color.value);
+
+    var tam = document.getElementsByName('tamFuente')[0];
+    var tamelegido = tam[tam.selectedIndex].value;
+
+    // aplicamos el color y el tamaño a todo el documento
+    document.documentElement.style.fontSize = parseFloat(tamelegido) + "rem";
+    document.documentElement.style.setProperty("--colorNormal", color.value);
+    document.getElementById('mensaje').innerHTML = "APLICADO";
+}
