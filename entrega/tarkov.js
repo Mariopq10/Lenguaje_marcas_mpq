3548b5ee1df055f42ea978ac9288c4e757f72d1c
Mariopq10 <mario.pq10@gmail.com>
2023-05-25 15:12:10 +0200

    arreglo tamaño y letra

--- entrega/tarkov.js
+++ entrega/tarkov.js
@@ -23,15 +23,58 @@ function guardarNombreUsuario() {
 }
 
 function aplicarCambios() {
-    // mostramos en consola los valores recogidos en la pagina
-    var color = document.getElementsByName('colorElegido')[0];
-    console.log('color elegido ' + color.value);
+    // Obtener el color seleccionado
+    var color = document.getElementsByName('colorElegido')[0].value;
 
+    // Obtener el tamaño seleccionado
     var tam = document.getElementsByName('tamFuente')[0];
-    var tamelegido = tam[tam.selectedIndex].value;
+    var tamElegido = tam[tam.selectedIndex].value;
 
-    // aplicamos el color y el tamaño a todo el documento
-    document.documentElement.style.fontSize = parseFloat(tamelegido) + "rem";
-    document.documentElement.style.setProperty("--colorNormal", color.value);
-    document.getElementById('mensaje').innerHTML = "Este ajuste solo sirve para el contenido de la página.";
+    // Guardar el tamaño y color seleccionados en localStorage
+    window.localStorage.setItem('tamFuenteGuardado', tamElegido);
+    window.localStorage.setItem('colorGuardado', color);
+
+    // Cambiar el color de los elementos
+    var elements = document.querySelectorAll('section p');
+    elements.forEach(function(element) {
+        element.style.color = color;
+    });
+
+    // Aplicar el tamaño a todo el documento
+    document.documentElement.style.fontSize = parseFloat(tamElegido) + 'rem';
+    document.documentElement.style.setProperty('--colorNormal', color);
+
+    document.getElementById('mensaje').innerHTML = 'Este mensaje muestra el tipo de tamaño y color del texto';
+    console.log(color);
+    console.log(tamElegido);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Leer el tamaño y color guardados en localStorage
+    var tamGuardado = window.localStorage.getItem('tamFuenteGuardado');
+    var colorGuardado = window.localStorage.getItem('colorGuardado');
+
+    if (tamGuardado) {
+        // Aplicar el tamaño guardado al estilo CSS
+        document.documentElement.style.fontSize = parseFloat(tamGuardado) + 'rem';
+
+        // Actualizar la selección en el elemento <select>
+        var tam = document.getElementsByName('tamFuente')[0];
+        for (var i = 0; i < tam.options.length; i++) {
+            if (tam.options[i].value === tamGuardado) {
+                tam.selectedIndex = i;
+                break;
+            }
+        }
+    }
+if (colorGuardado) {
+        // Cambiar el color de los elementos
+        var elements = document.querySelectorAll('section p');
+        elements.forEach(function(element) {
+            element.style.color = colorGuardado;
+        });
+
+        // Aplicar el color al estilo CSS
+        document.documentElement.style.setProperty('--colorNormal', colorGuardado);
+    }
+});
